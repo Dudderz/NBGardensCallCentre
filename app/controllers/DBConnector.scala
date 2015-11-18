af9ddc282050ea55ae29f722dadc0ae59f5d043c
Dudderz <tom.dudley5@gmail.com>
2015-11-18 17:30:29 +0000

    end of day submission

--- app/controllers/DBConnector.scala
+++ app/controllers/DBConnector.scala
@@ -1,8 +1,165 @@
-package controllers
+package com.qa.databaseconnector
+
+import java.sql.DriverManager
+import java.sql.Connection
+import java.sql.SQLException
+import java.sql.ResultSet
 
 /**
-  * Created by tdudley on 18/11/2015.
-  */
+  * @author tdudley
+  *
+  * Scala JDBC Connector
+  *
+  * */
 class DBConnector {
+
+  var connection:Connection = null
+
+  /**
+    * @return : connection - connection to the mySQL database
+    *
+    * Creates a connection to the sql database and
+    * once connected, returns the connection
+    *
+    */
+
+  def connect() : Connection =
+  {
+    val driver = "com.mysql.jdbc.Driver"
+    val url = "jdbc:mysql://localhost:3306/mydb"
+    val username = "root"
+    val password = "user"
+
+    try {
+
+      //Makes the initial connection
+      Class forName(driver)
+      connection = DriverManager getConnection(url, username, password)
+
+
+    } catch {
+      case e : Throwable => e printStackTrace
+    }
+
+    connection
+  }
+
+  /**
+    * @param : query the sql query that the will find all of the items
+    * within the given table
+    * @result : ResultSet this contains the results from the sql query
+    */
+
+  def findAllSQL(query : String) : ResultSet =
+  {
+    try{
+
+      val connection : Connection = connect
+
+      val statement = connection createStatement
+      val resultSet = statement executeQuery(query)
+
+      resultSet
+
+    }
+    catch
+      {
+        case e : SQLException => println("SQL Exception")
+          null
+      }
+  }
+
+  /**
+    * @param : query the sql query that the will find all of the items
+    * within the given table
+    * @param : id the given id that will be searched for within the database
+    * @result : ResultSet this contains the results from the sql query
+    */
+
+  def runSQLQuery(query : String, id : Int) : ResultSet =
+  {
+
+    try{
+
+      val connection : Connection = connect
+
+      val statement = connection createStatement
+      val resultSet = statement executeQuery(query + id)
+
+      resultSet
+
+    }
+    catch
+      {
+        case e : SQLException => println("SQL Exception")
+          null
+      }
+
+  }
+
+  /**
+    * @param : update part of the sql query that will make up the update query for
+    * within the given table
+    * @param : where part of the sql query that will make up the update query for
+    * within the given table
+    * @param : id1 the id that will be updated
+    * @param : id2 the id that will be used to search for the item in question
+    * @result : ResultSet this contains the results from the sql query
+    */
+
+  def runSQLUpdateInt(update : String, where : String,  id1 : Int, id2 : Int) : Unit =
+  {
+
+    try{
+
+      val connection : Connection = connect
+
+      val statement = connection createStatement
+      val resultSet = statement executeUpdate(update + id1 + where + id2)
+
+    }
+    catch
+      {
+        case e : SQLException => println("SQL Exception")
+      }
+
+  }
+
+  /**
+    * @param : update part of the sql query that will make up the update query for
+    * within the given table
+    * @param : where part of the sql query that will make up the update query for
+    * within the given table
+    * @param : id1 the id that will be updated
+    * @param : id2 the id that will be used to search for the item in question
+    * @result : ResultSet this contains the results from the sql query
+    */
+
+  def runSQLUpdateString(update : String, where : String, status : String, id : Int) : Unit =
+  {
+    try{
+
+      val connection : Connection = connect
+
+      val statement = connection createStatement
+      val resultSet = statement executeUpdate(update + status + where + id)
+
+    }
+    catch
+      {
+        case e : SQLException => println("SQL Exception")
+      }
+  }
+
+  /*
+   * closes the connection to the database
+   */
+  def closeConnection() : Unit =
+  {
+    if(connection != null)
+    {
+      connection close
+    }
+  }
 
 }
--- app/model/Address.scala
+++ app/model/Address.scala
@@ -3,6 +3,19 @@ package model
 /**
   * Created by tdudley on 18/11/2015.
   */
-class Address(val houseNumber : Int, val firstLine : String, val town : String, val postcode : String) {
+case class Address
+  (val houseNumber : Int, val firstLine : String, val town : String, val postcode : String) {
 
+}
+
+object Address {
+
+  var addresses = Set{
+    Address(1, "High Street", "Stoke", "ST6 5TT"),
+    Address(2, "High Street", "Stoke", "ST6 5TT"),
+    Address(3, "High Street", "Stoke", "ST6 5TT"),
+    Address(1, "High Street", "Stoke", "ST6 5TT")
+  }
+
+  def findAll = addresses.toList.sortBy(_.houseNumber)
 }
